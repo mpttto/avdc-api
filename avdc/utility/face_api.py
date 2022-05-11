7c161e0696d0fb4683f8127cfe48a472c55c060d
mpttto <105357517+mpttto@users.noreply.github.com>
2022-05-11 20:57:00 +0800

    Update face_api.py

--- avdc/utility/face_api.py
+++ avdc/utility/face_api.py
@@ -4,10 +4,33 @@
 class FaceAPIError(Exception):
     pass
 
-
 def detect_faces(content: bytes):
     """Detects faces in an image."""
-    return [tuple((1112,407)),tuple((1946,407)),tuple((1946,1270)),tuple((1112,1270))]
+    vertices = [{
+                "x": 1112,
+                "y": 407
+              },
+              {
+                "x": 1946,
+                "y": 407
+              },
+              {
+                "x": 1946,
+                "y": 1270
+              },
+              {
+                "x": 1112,
+                "y": 1270
+              }]
+    return [tuple(
+        (vertex.x, vertex.y)
+         for vertex in vertices
+    )]
+    # tuple(), tuple((1946, 407)), tuple((1946, 1270)), tuple((1112, 1270))
+    # return [tuple(
+    #     (vertex.x, vertex.y)
+    #               for vertex in face.bounding_poly.vertices)
+    #         for face in response.face_annotations]
 
 
 
